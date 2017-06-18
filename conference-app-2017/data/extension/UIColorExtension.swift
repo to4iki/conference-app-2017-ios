69b11723b156d3d606cc3664603e481c0615ed4e
tsk takezawa <tsk.take815@gmail.com>
2017-06-18 19:30:32 +0900

    Add app color definition

---
+++ conference-app-2017/data/extension/UIColorExtension.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension UIColor {
+    struct Builderscon {
+        private init() {}
+        /// #e95433
+        static let themeRed = #colorLiteral(red: 0.9232247472, green: 0.326004535, blue: 0.1610974669, alpha: 1)
+        /// #5eb6e8
+        static let themeBlue = #colorLiteral(red: 0.3515082598, green: 0.7104230523, blue: 0.9186832905, alpha: 1)
+        /// #000000
+        static let themeBlack = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+}
