2a8f148e9f39a488acd4b33d63bb70ed4942e005
tsk takezawa <tsk.take815@gmail.com>
2017-07-11 03:51:46 +0900

    Add dummy conference json

--- conference-app-2017/domain/dummy/DummyDataFactory.swift
+++ conference-app-2017/domain/dummy/DummyDataFactory.swift
@@ -4,14 +4,26 @@ import Himotoki
 
 // TODO: deleted
 struct DummyDataFactory {
-    private static let rawSessions = Bundle.main.path(forResource: "sessions", ofType:"json")!
+    private static let sessionsJsonPath = Bundle.main.path(forResource: "sessions", ofType:"json")!
+    private static let conferenceJsonPath = Bundle.main.path(forResource: "conference", ofType:"json")!
 
     static func sessions() -> [Session] {
-        let handle = FileHandle(forReadingAtPath: rawSessions)
+        let handle = FileHandle(forReadingAtPath: sessionsJsonPath)
         guard let data = handle?.readDataToEndOfFile() else { fatalError("load error") }
         do {
             let json = try JSONSerialization.jsonObject(with: data, options: [])
             return try decodeArray(json)
+        } catch {
+            fatalError("error: \(error.localizedDescription)")
+        }
+    }
+
+    static func conference() -> Conference {
+        let handle = FileHandle(forReadingAtPath: conferenceJsonPath)
+        guard let data = handle?.readDataToEndOfFile() else { fatalError("load error") }
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            return try Conference.decodeValue(json)
         } catch {
             fatalError("error: \(error.localizedDescription)")
         }
