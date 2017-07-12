1253c7b45da018aa51f8d8a437b700ed834f347e
tsk takezawa <tsk.take815@gmail.com>
2017-07-12 20:35:08 +0900

    Render markdown to session.abstract

--- conference-app-2017/presentation/viewController/SessionViewController.swift
+++ conference-app-2017/presentation/viewController/SessionViewController.swift
@@ -1,20 +1,24 @@
 import UIKit
 import Kingfisher
+import MarkdownView
 import OctavKit
 import Then
 
 final class SessionViewController: UIViewController {
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var startToEndLabel: UILabel!
-    @IBOutlet weak var avatarImageView: UIImageView!
-    @IBOutlet weak var nicknameLabel: UILabel!
-    @IBOutlet weak var abstractTextView: UITextView!
+    @IBOutlet fileprivate weak var contentView: UIView!
+    @IBOutlet fileprivate weak var indicatorView: UIActivityIndicatorView!
+    @IBOutlet fileprivate weak var titleLabel: UILabel!
+    @IBOutlet fileprivate weak var startToEndLabel: UILabel!
+    @IBOutlet fileprivate weak var avatarImageView: UIImageView!
+    @IBOutlet fileprivate weak var nicknameLabel: UILabel!
+    @IBOutlet fileprivate weak var abstractMarkdownView: MarkdownView!
+    @IBOutlet fileprivate weak var abstractMarkdownViewHeight: NSLayoutConstraint!
 
     fileprivate var session: Session!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup(session: session)
+        setupLayout(session: session)
     }
 }
 
@@ -27,11 +31,31 @@ extension SessionViewController {
 }
 
 extension SessionViewController {
-    fileprivate func setup(session: Session) {
+    fileprivate func setupLayout(session: Session) {
+        hideLayout()
         titleLabel.text = session.title
         startToEndLabel.text = "\(DateFormatter.year.string(from: session.startsOn))"
         avatarImageView.kf.setImage(with: session.speaker.avatarURL)
         nicknameLabel.text = session.speaker.nickname
-        abstractTextView.text = session.abstract
+        abstractMarkdownView.load(markdown: session.abstract)
+        abstractMarkdownView.isScrollEnabled = false
+        abstractMarkdownView.onRendered = { [weak self] (height: CGFloat) in
+            guard let strongSelf = self else { return }
+            strongSelf.showLayout()
+            strongSelf.abstractMarkdownViewHeight.constant = height
+            strongSelf.view.setNeedsLayout()
+        }
+    }
+
+    private func showLayout() {
+        indicatorView.stopAnimating()
+        indicatorView.isHidden = true
+        contentView.isHidden = false
+    }
+
+    private func hideLayout() {
+        indicatorView.startAnimating()
+        indicatorView.isHidden = false
+        contentView.isHidden = true
     }
 }
