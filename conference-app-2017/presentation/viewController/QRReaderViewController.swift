a39f765c9ca4c75e6ad678a4dbec3ff1f68a0195
morizooo <knnmrs999@gmail.com>
2017-07-16 13:06:03 +0900

    add QRReader sample

--- conference-app-2017/presentation/viewController/QRReaderViewController.swift
+++ conference-app-2017/presentation/viewController/QRReaderViewController.swift
@@ -1,8 +1,51 @@
+import AVFoundation
 import UIKit
+import QRCodeReader
 
-final class QRReaderViewController: UIViewController {
+final class QRReaderViewController:  UIViewController, QRCodeReaderViewControllerDelegate {
+    lazy var reader = QRCodeReaderViewController(builder: QRCodeReaderViewControllerBuilder {
+        $0.reader          = QRCodeReader(metadataObjectTypes: [AVMetadataObjectTypeQRCode])
+        $0.showTorchButton = true
+    })
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        do {
+            if try QRCodeReader.supportsMetadataObjectTypes() {
+                reader.modalPresentationStyle = .formSheet
+                reader.delegate               = self
+                
+                reader.completionBlock = { (result: QRCodeReaderResult?) in
+                    if let result = result {
+                        print("Completion with result: \(result.value) of type \(result.metadataType)")
+                    }
+                }
+                
+                present(reader, animated: true, completion: nil)
+            }
+        } catch let error as NSError {
+            //エラー処理
+        }
+    }
+    
+    func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
+        reader.stopScanning()
+        
+        dismiss(animated: true) { [weak self] in
+            //読み取った後の処理
+            //result.valueで取得
+        }
+    }
+    
+    func reader(_ reader: QRCodeReaderViewController, didSwitchCamera newCaptureDevice: AVCaptureDeviceInput) {
+        if let cameraName = newCaptureDevice.device.localizedName {
+            print("Switching capturing to: \(cameraName)")
+        }
+    }
+    
+    func readerDidCancel(_ reader: QRCodeReaderViewController) {
+        reader.stopScanning()
+        
+        dismiss(animated: true, completion: nil)
     }
 }
