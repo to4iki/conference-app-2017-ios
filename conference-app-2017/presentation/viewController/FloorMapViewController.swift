1790d0df63dd51d98b643e84d03e9db4cc51c720
tsk takezawa <tsk.take815@gmail.com>
2017-07-18 23:40:43 +0900

    Fix FloorMap page
    - scalinga when doubleTap

--- conference-app-2017/presentation/viewController/FloorMapViewController.swift
+++ conference-app-2017/presentation/viewController/FloorMapViewController.swift
@@ -6,28 +6,40 @@ final class FloorMapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNoTitleBackButton()
         scrollView.delegate = self
+        let doubleTapGesture = UITapGestureRecognizer(target: self, action: .didImageDoubleTap)
+        doubleTapGesture.numberOfTapsRequired = 2
+        imageView.addGestureRecognizer(doubleTapGesture)
     }
 }
 
 extension FloorMapViewController {
-    fileprivate func updateScrollInset() {
-        scrollView.contentInset = UIEdgeInsets(
-            top: max((scrollView.frame.height - imageView.frame.height)/2, 0),
-            left: max((scrollView.frame.width - imageView.frame.width)/2, 0),
-            bottom: 0,
-            right: 0
-        )
+    func didImageDoubleTap(gesture: UITapGestureRecognizer) {
+        guard gesture.state == UIGestureRecognizerState.ended else { return }
+        if scrollView.zoomScale < scrollView.maximumZoomScale {
+            let zoomedRect = zoomRectForScale(scrollView.maximumZoomScale, center: gesture.location(in: gesture.view))
+            scrollView.zoom(to: zoomedRect, animated: true)
+        } else {
+            scrollView.setZoomScale(1.0, animated: true)
+        }
+    }
+
+    private func zoomRectForScale(_ scale: CGFloat, center: CGPoint) -> CGRect {
+        let size = CGSize(width: scrollView.frame.size.width / scale, height: scrollView.frame.size.height / scale)
+        let origin = CGPoint(x: center.x - size.width / 2.0, y: center.y - size.height / 2.0)
+        return CGRect(origin: origin, size: size)
     }
 }
 
 // MARK: - UIScrollViewDelegate
 extension FloorMapViewController: UIScrollViewDelegate {
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        updateScrollInset()
-    }
-
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
     }
+}
+
+// MARK: - Selector
+private extension Selector {
+    static let didImageDoubleTap = #selector(FloorMapViewController.didImageDoubleTap)
 }
