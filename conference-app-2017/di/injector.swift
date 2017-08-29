37ae3e3d50d7c26e50ebeedce6d02fece3297d40
tsk takezawa <tsk.take815@gmail.com>
2017-08-29 23:00:23 +0900

    Disable Swinject Logging
    currently not supported when using storyboard extension
    see also: Swinject/Swinject#213

--- conference-app-2017/di/injector.swift
+++ conference-app-2017/di/injector.swift
@@ -1,7 +1,11 @@
+import Swinject
 import SwinjectStoryboard
 
 extension SwinjectStoryboard {
     static func setup() {
+        // disable since doesn't work with storyboard plugin
+        Container.loggingFunction = nil
+        
         let container = defaultContainer
         DataContainer.register(container: container)
         DomainContainer.register(container: container)
